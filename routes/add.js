4cc96751a8cc04422a2b62561f1c9274a1d10f8f
Amaya Mali <asmali@ucsd.edu>
2020-02-06 11:19:48 -0800

    creating picture and fetch data

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,23 @@
 var data = require("../data.json");
 
+
+
+
 exports.addFriend = function(request, response) {    
 	// Your code goes here
+	var findname = request.query.name;
+	var finddescription = request.query.description; 
+
+	var friendplus = {
+		"name": findname,
+		"description": finddescription,
+		"imageURL": "http://lorempixel.com/400/400/people",
+		
+
+	};
+	data.friends.push(friendplus);
+	response.render('index', data);
+	
+	
+	
  }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 // Get all of our friend data
 var data = require('../data.json');
+// response.render('index',data)
+
 
 exports.view = function(request, response){
 	console.log(data);
-	response.render('index');
+	response.render('index', data);
 };
